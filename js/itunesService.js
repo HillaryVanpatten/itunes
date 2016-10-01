3ece62152c0e2c3fb684ad8558fb1369d3116056
HillaryVanpatten <hillaryvanpatten@gmail.com>
2016-10-01 10:09:59 -0600

    finished

--- js/itunesService.js
+++ js/itunesService.js
@@ -1,12 +1,42 @@
-angular.module('itunes').service('itunesService', function($http, $q){
-  //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
-  //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
+  angular.module('itunes').service('itunesService', function($http, $q){
+   //This service is what will do the 'heavy lifting' and get our data from the iTunes API.
+   //Also note that we're using a 'service' and not a 'factory' so all your methods you want to call in your controller need to be on 'this'.
 
-  //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
-  //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
-  //Note that in the above line, artist is the parameter being passed in. 
-  //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
+   //Write a method that accepts an artist's name as the parameter, then makes a 'JSONP' http request to a url that looks like this
+   //https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+   //Note that in the above line, artist is the parameter being passed in.
+   //You can return the http request or you can make your own promise in order to manipulate the data before you resolve it.
 
-    //Code here
-    
-});
+     //Code here
+     this.getSongData= function(artist) {
+       var deferred = $q.defer();
+       $http({
+         method: 'JSONP',
+         url: 'https://itunes.apple.com/search?term=' + artist + '&callback=JSON_CALLBACK'
+       }).then(function(response){
+         var filterData = response.data.results;
+         var responseData = [];
+
+         for (var i = 0; i < filterData.length; i++){
+           var obj = {
+             Artist: filterData[i].artistName,
+             Play: filterData[i].previewUrl,
+             Collection: filterData[i].collectionName,
+             AlbumArt: filterData[i].artworkUrl100,
+             Type: filterData[i].kind,
+             CollectionPrice: filterData[i].collectionPrice
+           }
+           responseData.push(obj);
+
+         }
+         deferred.resolve(responseData)
+
+         console.log(11111,response.data.results)
+         return response.data.results;
+
+       })
+       return deferred.promise;
+
+     }
+
+  });
